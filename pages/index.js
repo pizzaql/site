5cdaaf230032730b39f53372c9941100043fe32a
Antoni Kepinski <xxczaki@pm.me>
2019-08-31 16:49:03 +0200

    Page redesign :unicorn:

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import GithubButton from '../components/github-button';
 import DocsButton from '../components/docs-button';
 import Description from '../components/description';
 import ButtonGroup from '../components/button-group';
+import Card from '../components/card';
+import Footer from '../components/footer';
 
 const Index = () => (
 	<Container>
@@ -19,6 +21,29 @@ const Index = () => (
 			<br/>
 			<DocsButton/>
 		</ButtonGroup>
+		<Card
+			src="../static/images/undraw_react.svg"
+			title="Built with React"
+			text="Code is maintainable & easy to understand"
+		/>
+		<Card
+			src="../static/images/undraw_open_source.svg"
+			title="Open Source"
+			text="All code is available on our Github repository"
+		/>
+		<Card
+			src="../static/images/undraw_speed.svg"
+			title="Fast"
+			text="Focused on high performance"
+		/>
+		<Card
+			src="../static/images/undraw_stripe.svg"
+			title="Online payments"
+			text="Stripe is supported out-of-the-box"
+		/>
+		<Footer>
+			<p>MIT. Copyright (c) <a href="https://kepinski.me" target="_blank" rel="noopener noreferrer">Antoni Kepinski</a></p>
+		</Footer>
 	</Container>
 );
 
