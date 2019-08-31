98906189ad74766bf66f37437a80d2aa3094b579
Antoni Kepinski <xxczaki@pm.me>
2019-08-31 16:49:20 +0200

    Add card component

---
+++ components/card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    width: 45vw;
+    padding-bottom: 5em;
+`;
+
+const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30em;
+`;
+
+const Img = styled.img`
+    width: 18em;
+`;
+
+const Card = ({src, title, text}) => (
+	<Wrapper>
+		<Img alt={title} importance="low" decoding="async" loading="lazy" src={src} draggable={false}/>
+		<br/>
+		<Description>
+			<h2>{title}</h2>
+			<p>{text}</p>
+		</Description>
+	</Wrapper>
+);
+
+Card.propTypes = {
+	src: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired
+};
+
+export default Card;
